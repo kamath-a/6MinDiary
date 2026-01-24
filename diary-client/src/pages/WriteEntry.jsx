c63db1b88a7a896df46167887922746a4b887b54
Agnetha <agnethakamath@gmail.com>
2026-01-24 18:04:37 +0000

    added a Flask backend!

--- diary-client/src/pages/WriteEntry.jsx
+++ diary-client/src/pages/WriteEntry.jsx
@@ -4,11 +4,13 @@ import { EveningEntry } from "../components/EveningEntry";
 import { Button } from "../components/Button";
 
 export function WriteEntry() {
+  const currentHour = new Date().getHours();
+  const isMorning = currentHour < 12;
+
   return (
     <>
       <h1>This is where you write a new entry!</h1>
-      <MorningEntry />
-      <EveningEntry />
+      {isMorning ? <MorningEntry /> : <EveningEntry />}
       <Button />
       <Link to="/dashboard">Back to dashboard</Link>
     </>
